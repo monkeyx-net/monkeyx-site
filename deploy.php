4147b9e00625ebda62a172d09491a2b38c36f217
monkeyx-net <tim@monkeyx.net>
2026-07-16 14:25:54 +0100

    d test

--- deploy.php
+++ deploy.php
@@ -5,5 +5,5 @@ if (!hash_equals($sig, $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '')) {
     http_response_code(403);
     exit;
 }
-$output = shell_exec('git -C /var/www/html pull 2>&1');
+$output = shell_exec('sudo -n -u tim git -C /srv/http pull 2>&1');
 echo $output;
